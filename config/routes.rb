58b78eedc594d7f15a639e78269fb48ffb8f5f36
Cristian Zambrano <criszamcham@gmail.com>
2023-08-09 16:13:12 -0400

    Add recipe controller and routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  get '/users/:user_id/recipes/:id', to: 'recipes#show', as: 'user_recipe'
+
   resources :ingredients
 end
