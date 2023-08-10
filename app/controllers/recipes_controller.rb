b91c24135e44832c0b4d0ead88efc26fbf6f82b3
Cristian Zambrano <criszamcham@gmail.com>
2023-08-09 21:37:20 -0400

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -46,5 +46,5 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
-  end  
+  end
 end
